f49baccd4093738ecffb90768ad171277f54be32
DanielHernandez37 <710113@student.dusd.net>
2023-03-16 20:50:41 -0700

    challenge 2

---
+++ BasicGameplayHernandezP4/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool shotAllowed = true;
+
+    
+    IEnumerator MakeWait() {
+        shotAllowed = false;
+        yield return new WaitForSeconds(.5f);
+        shotAllowed = true;
+    }
+
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Space) && shotAllowed)
+        {
+           
+            StartCoroutine(MakeWait());
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ BasicGameplayHernandezP4/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -14;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   
+    void SpawnRandomBall()
+    {
+      
+        int ballIndex  = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        
+        float minSpawnInterval = 3.0f;
+        float maxSpawnInterval = 5.0f;
+        float spawnInterval;
+
+        spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+
+}
